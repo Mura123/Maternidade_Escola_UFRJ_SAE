d5048f45981ae0c9455d36ff2d89741a0e04d423
Mura123 <lucasmurakami123@gmail.com>
2020-06-02 22:05:48 -0300

    First commit
    First screen v0.1
    Header screen v0.01

---
+++ Maternidade_Escola_UFRJ_SAE/Maternidade_Escola_UFRJ_SAE/Cabecalho.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Maternidade_Escola_UFRJ_SAE
+{
+    /// <summary>
+    /// Lógica interna para Window1.xaml
+    /// </summary>
+    public partial class Cabecalho : Window
+    {
+        public string TipoPaciente { get; set; }
+        public Cabecalho(string paciente)
+        {
+            InitializeComponent();
+
+            TipoPaciente = paciente;
+            Teste.Content = paciente;
+        }
+    }
+}
---
+++ Maternidade_Escola_UFRJ_SAE/Maternidade_Escola_UFRJ_SAE/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Maternidade_Escola_UFRJ_SAE
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AbreCabecalho(object sender, RoutedEventArgs e)
+        {
+            string paciente = "";
+            if (Gestante.IsChecked == true)
+            {
+                paciente = "gestante";
+            }
+            else if (Puerpera.IsChecked == true)
+            {
+                paciente = "puerpera";
+            }
+            else if (RecemNascido.IsChecked == true)
+            {
+                paciente = RecemNascido.Name;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, marque uma opção antes de prosseguir.");
+            }
+
+            if (!string.IsNullOrEmpty(paciente))
+            {
+                Cabecalho cabecalho = new Cabecalho(paciente);
+                cabecalho.Show();
+                this.Close();
+            }
+        }
+    }
+}
