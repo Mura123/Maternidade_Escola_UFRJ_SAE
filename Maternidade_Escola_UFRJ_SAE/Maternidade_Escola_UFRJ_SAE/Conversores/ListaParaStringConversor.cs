544cea74723dc74bab442827fafc6ed839259cbc
xhugo92 <hugofrn1992@hotmail.com>
2020-07-03 10:06:30 -0300

    Conversor adicionado

---
+++ Maternidade_Escola_UFRJ_SAE/Maternidade_Escola_UFRJ_SAE/Conversores/ListaParaStringConversor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using static System.Collections.Generic.Dictionary<string, string>;
+
+namespace Maternidade_Escola_UFRJ_SAE.Conversores
+{
+    class ListaParaStringConversor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string colecao = "";
+            int i = 0;
+            if(value is KeyCollection chaves)
+            {
+                foreach(var aux in chaves)
+                {
+                    if(i==0)
+                    {
+                        colecao += aux;
+                        i++;
+                    }
+                    else
+                    {
+                        colecao += "," + aux;
+                    }
+                }
+                return colecao;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
